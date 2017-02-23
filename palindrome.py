f338b715a68555cf717cf5294b8dfcbed1960e9e
Graham Brown <GrahamDavidBrown90@gmail.com>
2017-02-22 20:36:08 -0500

    first_commit

--- palindrome.py
+++ palindrome.py
@@ -5,30 +5,33 @@ def is_palindrome(user_input):
     user_input = re.sub(r'[^A-Za-z"]', "", user_input).lower()
     length = len(user_input)
     if length % 2 != 0:
-        user_input = user_input[:int(length / 2)] + user_input[int((length / 2) + 1):]
-    first_half = user_input[0: int(length / 2)]
-    second_half = user_input[int(length / 2): int(length)]
-    rev_str = reverser(int(len(second_half)), list(second_half), "")
-    if first_half == rev_str:
+        user_input = user_input[:int(length / 2)] + user_input[int(length / 2) + 1:]
+    second_half = reverser(len(user_input[int(length / 2):]), list(user_input[int(length / 2):]), "")
+    if user_input[: int(length / 2)] == second_half:
         return True
     else:
         return False
 
 
-def reverser(count, second_half_list, rev_str):
+def reverser(count, second_half_list, second_half):
     if second_half_list != []:
-        rev_str += second_half_list.pop()
+        second_half += second_half_list.pop()
     if count <= 0:
-        return rev_str
+        return second_half
     count -= 1
-    return reverser(count, second_half_list, rev_str)
+    return reverser(count, second_half_list, second_half)
 
 
 def main():
-    user_input = input("enter some text to determine if it is a palindrome: ")
-    print(user_input)
-    print(is_palindrome(user_input))
-
+    playing = True
+    while playing == True:
+        user_input = input("enter some text to determine if it is a palindrome(QQ to quit): ")
+        if user_input == "QQ": #Working as intended
+            playing = False
+        if is_palindrome(user_input):
+            print(user_input + " is a palindrome.")
+        else:
+            print(user_input + " is not a palindrome.")
 
 if __name__ == '__main__':
     main()
